b9a34cb94d7259e91485b6bf0cbdaa89e4ccd3b3
rashel68 <rashel341068@gmail.com>
2022-09-12 23:35:42 +0600

    applicant component updated

--- src/components/Applicant/Applicant.js
+++ src/components/Applicant/Applicant.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
+import AdmitCard from '../AdmitCard/AdmitCard';
 
 const Applicant = () => {
     const [applicants, setApplicants] = useState([]);
@@ -18,28 +19,31 @@ const Applicant = () => {
     };
     return (
         <div style={adminStyle}>
-            <h2>All Applicant</h2>
+            {
+                applicants.map(applicant => <h2 key={applicant._id} style={{ color: '#277BC0' }}>{applicant.applicantName} Info</h2>)
+            }
             <table style={{ margin: '0 auto', border: '1px solid #025c3b', borderCollapse: 'collapse', textAlign: 'left' }} border="1px solid" cellSpacing="0" cellPadding="4">
                 <tbody>
                     <tr style={{ fontWeight: 'bold' }}>
                         <td>SL</td>
-                        <td>ID</td>
+                        <td>Phone Number</td>
                         <td>Name</td>
                         <td>Father's Name</td>
                         <td>Post Name</td>
+                        <td>Application Form</td>
                     </tr>
                     {
                         applicants.map(applicant => <tr key={applicant._id}>
                             <td>{applicants.indexOf(applicant) + 1}</td>
-                            <td><Link to={`/admitCard/${applicant._id}`}>{applicant._id.slice(13)}</Link></td>
+                            <td>{applicant.p_number}</td>
                             <td>{applicant.applicantName}</td>
                             <td>{applicant.fname}</td>
                             <td>{applicant.postName}</td>
+                            <td style={{ textAlign: 'center' }}><Link to={`/admitCard/${applicant._id}`} style={{ textDecoration: 'none' }}>Download Now</Link></td>
                         </tr>)
                     }
                 </tbody>
             </table>
-
         </div>
     );
 };
